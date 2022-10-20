519534676bd6539d1f845fc22a03e8c5db9e3e7f
s64309010033 <s64309010033@ptnicec.ac.th>
2022-10-20 15:41:09 +0700

    ส่ง function_string

---
+++ function_string.php
@@ -0,0 +1,22 @@
+<?php
+
+    $name = "Wilaiwan Khaosaart";
+    echo strlen($name);
+    echo "<br>";
+    $word = "I love you";
+    echo str_word_count($word);
+    echo "<br>";
+    echo strrev($name);
+    echo "<br>";
+    echo strpos($word,"love");
+    echo "<br>";
+    echo str_replace("love","hate",$word);
+    echo "<br>";
+    echo strtoupper($word);
+    echo "<br>";
+    echo strtolower($word);
+    echo "<br>";
+    echo substr($word,-3);
+    echo "<br>";
+    echo substr($word,2,-4);
+?>

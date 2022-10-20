25d9917d0fb363ed7e32aa9d64c000ec4c8f84bb
s64309010033 <s64309010033@ptnicec.ac.th>
2022-10-20 15:38:12 +0700

    ไฟล์ variabla/test

---
+++ test.php
@@ -0,0 +1,26 @@
+<?php
+
+    echo "Wilaiwan Khaosaart<br>";
+    //echo "Thailand";
+    #echo "Asia";
+    /*
+    echo "World <br>";
+    echo "Universe <br>";
+    */
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TEST PHP</title>
+</head>
+<body>
+    Information Technology <br>
+    <?php
+        echo "Pattani:"
+    ?>
+</body>
+</html>
---
+++ variable.php
@@ -0,0 +1,22 @@
+<?php
+
+    $fname = 2;
+    $a1111 = 4;
+    $_test01 = 3.33;
+    $Age = 100;
+    $age = 99;
+    $fullname = "Wilaiwan Khaosaart";
+    $prefix = "Mr.";
+
+    echo $fname."<br>";
+    echo $a1111;
+    echo "<br>".$Age."<br>";
+    echo $prefix.$fullname."<br>";
+    echo $Age + $age."<br>";
+    echo $Age." + ".$age." = ".($Age + $age);
+    print "<br>".$_test01;
+    
+
+
+
+?>
